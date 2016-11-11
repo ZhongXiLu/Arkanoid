2912aceff28165f1c97fb5a4f72a6479f850a1fa
ZhongXiLu <zhongxilu@hotmail.com>
2016-11-11 13:41:34 +0100

    Implemented some methods of Player

--- src/arkanoid/game_logic/player/player.cpp
+++ src/arkanoid/game_logic/player/player.cpp
@@ -1,7 +1,24 @@
 
 #include "player.h"
+#include "../math/vector2D.h"
 
 #include <iostream>
 
 using namespace std;
 
+Player::Player() : playerPos(0, 0) {}
+
+Player::Player(int x, int y) : playerPos(x, y) {}
+
+Vector2D Player::getPos() const {
+	return playerPos;
+}
+
+bool Player::move(Vector2D newPos) {
+
+	// TBI: Check if new playerPos is in Grid.
+
+	playerPos = playerPos + newPos;
+
+	return true;
+}
--- src/arkanoid/game_logic/player/player.h
+++ src/arkanoid/game_logic/player/player.h
@@ -1,12 +1,51 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "../math/vector2D.h"
+
 #include <iostream>
 
 using namespace std;
 
 class Player {
+private:
+	Vector2D playerPos;	///< The current position of the Player (in the 9x7 grid).
 
+public:
+
+	/**
+	* Default Constructor.
+	*
+	* Initialise the Player at origin ([0, 0]) in the World.
+	*/
+	Player();
+
+	/*
+	* Constructor
+	*
+	* Initialise the Player at a specific position in the World.
+	*
+	* @param x 	The x position of the Player.
+	* @param y 	The y position of the Player.
+	*/
+	Player(int x, int y);
+
+	/*
+	* Get the current position of the Player.
+	*
+	* @return The current position of the Player (in a Vector2D).
+	*/
+	Vector2D getPos() const;
+
+	/*
+	* Moves the Player.
+	*
+	* @param newPos	The Vector2D over which the Player will be translated.
+	*
+	* @return True if move was valid and made, otherwise False.
+	*/
+	bool move(Vector2D newPos);
+	
 };
 
 #endif /* PLAYER_H */
