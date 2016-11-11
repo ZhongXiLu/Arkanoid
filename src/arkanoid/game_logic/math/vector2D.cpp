69ea9016b86bcd57daa1186446dac9fc45157fde
ZhongXiLu <zhongxilu@hotmail.com>
2016-11-11 13:40:32 +0100

    Added Vector2D class

---
+++ src/arkanoid/game_logic/math/vector2D.cpp
@@ -0,0 +1,19 @@
+
+#include "vector2D.h"
+
+#include <iostream>
+
+using namespace std;
+
+Vector2D::Vector2D() {};
+
+Vector2D::Vector2D(int newX, int newY) : x(newX), y(newY) {}
+
+Vector2D Vector2D::operator+(const Vector2D &other) {
+	return Vector2D(x + other.x, y + other.y);
+}
+
+void Vector2D::operator=(const Vector2D &other) {
+	x = other.x;
+	y = other.y;
+}
---
+++ src/arkanoid/game_logic/math/vector2D.h
@@ -0,0 +1,49 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+#include <iostream>
+
+using namespace std;
+
+class Vector2D {
+public:
+
+	int x;	///< The 'x' component of the Vector2D.
+	int y;	///< The 'y' component of the Vector2D.
+
+	/**
+	* Default Constructor.
+	*/
+	Vector2D();
+
+	/*
+	* Constructor.
+	*
+	* @param newX	The 'x' component of the new constructed Vector2D.
+	* @param newY	The 'y' component of the new constructed Vector2D.
+	*/
+	Vector2D(int newX, int newY);
+
+	/*
+	* '+' Overloader.
+	*
+	* Translates the Vector2D over the other Vector2D.
+	*
+	* @param other	The other Vector2D on which this Vector2D will be translated on.
+	*
+	* @return The result of the translation.
+	*/
+	Vector2D operator+(const Vector2D &other);
+
+	/*
+	* '=' Overloader.
+	*
+	* Asign new values to this Vector2D.
+	*
+	* @param other The Vector2D that contains the new values 'x' and 'y'.
+	*/
+	void operator=(const Vector2D &other);
+
+};
+
+#endif /* VECTOR2D_H */
