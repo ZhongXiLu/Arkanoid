c78d44578ab43456bd2dc1dff68a152aeb851a07
ZhongXiLu <zhongxilu@hotmail.com>
2016-11-13 11:11:19 +0100

    Added main documentation (main.cpp) + created sprites directory

--- src/arkanoid/game_gui/world.cpp
+++ src/arkanoid/game_gui/world.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
 namespace arkanoidSFML {
 
-	World::World() : player(4, 1, "player.png") {}
+	World::World() : player(4, 1, "sprites/player/player.png") {}
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,37 @@
 
 /**
 * \mainpage Arkanoid
+*
+* \section howToBuild How to build
+*
+* (in the project main directory)
+*
+* \code{.sh}
+* mkdir build
+* cd build
+* cmake ..
+* make
+* make install
+* cd ../bin
+* ./arkanoid
+* \endcode
+*
+* \section howToRun How to run
+*
+* \code{.sh}
+* ./arkanoid
+* \endcode
+*
+* \section controls Controls
+* <ul>
+* 	<li>Left arrow: move "one block" to the left</li>
+* 	<li>Right arrow: move "one block" to the right</li>
+* </ul>
+*
+* \section remarks Remarks
+* <ul>
+* 	<li>The directory "sprites" (in the project main directory) should be copied to the same directory of the executable. (sprites are needed for the program to work)</li>
+* </ul>
 */
 int main() {
 
